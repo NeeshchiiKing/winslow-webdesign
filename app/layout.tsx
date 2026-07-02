474ff88c72ecfc2fc23960f17b935c9db3a9e37c
Roberto Ortiz <robpam4ever@gmail.com>
2026-07-02 11:00:16 -0700

    Add: GDPR consent banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConsentBanner from './ConsentBanner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +38,10 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ConsentBanner />
+        {children}
+      </body>
     </html>
   );
 }
