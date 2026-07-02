f8cd352d52f9867686003e793f9ed45328a3d133
Roberto Ortiz <robpam4ever@gmail.com>
2026-07-02 11:14:01 -0700

    Fix: Correct ConsentBanner import case

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ConsentBanner from './ConsentBanner';
+import ConsentBanner from './consentbanner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
