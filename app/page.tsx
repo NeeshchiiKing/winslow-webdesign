42923a429b73954961920c8120aec91f6f017600
Roberto Ortiz <robpam4ever@gmail.com>
2026-07-03 04:20:44 -0700

    Fix: Remove config from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,3 @@ export function middleware(request: NextRequest) {
   
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ['/((?!api|_next|_static|public|favicon.ico).*)'],
-};
